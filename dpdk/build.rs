d04b398ed557c67558505dad46868a8bf79052b8
Gopa Kumar <gopakumarce@gmail.com>
2020-06-26 14:27:08 -0400

    go back to shared lib model

--- dpdk/build.rs
+++ dpdk/build.rs
@@ -1,27 +1,10 @@
 use std::env;
-use std::process::Command;
 
-// TODO: Need meson, ninja, tar, curl, unxz, gcc/linker stuff
-// Somehow check for presense of all this even before the build script
-// runs and bail out if something is missing
 fn main() {
     let out = env::var("OUT_DIR").unwrap();
     println!("DPDK Output directory is {}", &out);
 
-    // Download and build dpdk
-    let mut cmd = Command::new("./build.sh");
-    cmd.arg(&out);
-    if let Err(status) = cmd.status() {
-        println!("dpdk build failed with {}", status);
-        return;
-    }
-
-    println!(
-        "cargo:rustc-link-search=native={}/{}",
-        &out, "dpdk/build/lib/"
-    );
-    println!("cargo:rustc-link-lib=static=rte_eal");
-    println!("cargo:rustc-link-lib=static=rte_kvargs");
+    println!("cargo:rustc-link-lib=rte_eal");
+    println!("cargo:rustc-link-lib=rte_kvargs");
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=build.sh");
 }
