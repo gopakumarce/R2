b7fe41f6d72e1de2fc4fd61ad20818eb37e6a941
Gopa Kumar <gopakumarce@gmail.com>
2020-08-29 18:21:43 -0400

    link dynamically to dpdk libs

--- ffis/dpdk/build.rs
+++ ffis/dpdk/build.rs
@@ -4,10 +4,10 @@ fn main() {
     let out = env::var("OUT_DIR").unwrap();
     println!("DPDK Output directory is {}", &out);
 
-    println!("cargo:rustc-link-lib=rte_eal");
-    println!("cargo:rustc-link-lib=rte_kvargs");
-    println!("cargo:rustc-link-lib=rte_mempool");
-    println!("cargo:rustc-link-lib=rte_mbuf");
-    println!("cargo:rustc-link-lib=rte_ethdev");
+    println!("cargo:rustc-link-lib=dylib=rte_eal");
+    println!("cargo:rustc-link-lib=dylib=rte_kvargs");
+    println!("cargo:rustc-link-lib=dylib=rte_mempool");
+    println!("cargo:rustc-link-lib=dylib=rte_mbuf");
+    println!("cargo:rustc-link-lib=dylib=rte_ethdev");
     println!("cargo:rerun-if-changed=build.rs");
 }
